ae9334baacdbc3c5e88dcd6e179c018cfa7e6506
McLov1n <112742660+McLov1ns@users.noreply.github.com>
2023-06-15 09:43:16 +0700

    Add files via upload

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace test
+{
+    partial class TestForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.RadioButton answerRadioButton1;
+        private System.Windows.Forms.RadioButton answerRadioButton2;
+        private System.Windows.Forms.RadioButton answerRadioButton3;
+
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.okButton = new System.Windows.Forms.Button();
+            this.answerRadioButton1 = new System.Windows.Forms.RadioButton();
+            this.answerRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.answerRadioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Location = new System.Drawing.Point(38, 56);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(0, 13);
+            this.questionLabel.TabIndex = 0;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(50, 19);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(187, 13);
+            this.progressBar.TabIndex = 2;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(155, 184);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(41, 32);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // answerRadioButton1
+            // 
+            this.answerRadioButton1.AutoSize = true;
+            this.answerRadioButton1.Location = new System.Drawing.Point(41, 86);
+            this.answerRadioButton1.Name = "answerRadioButton1";
+            this.answerRadioButton1.Size = new System.Drawing.Size(14, 13);
+            this.answerRadioButton1.TabIndex = 4;
+            this.answerRadioButton1.TabStop = true;
+            this.answerRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // answerRadioButton2
+            // 
+            this.answerRadioButton2.AutoSize = true;
+            this.answerRadioButton2.Location = new System.Drawing.Point(41, 109);
+            this.answerRadioButton2.Name = "answerRadioButton2";
+            this.answerRadioButton2.Size = new System.Drawing.Size(14, 13);
+            this.answerRadioButton2.TabIndex = 4;
+            this.answerRadioButton2.TabStop = true;
+            this.answerRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // answerRadioButton3
+            // 
+            this.answerRadioButton3.AutoSize = true;
+            this.answerRadioButton3.Location = new System.Drawing.Point(41, 132);
+            this.answerRadioButton3.Name = "answerRadioButton3";
+            this.answerRadioButton3.Size = new System.Drawing.Size(14, 13);
+            this.answerRadioButton3.TabIndex = 4;
+            this.answerRadioButton3.TabStop = true;
+            this.answerRadioButton3.UseVisualStyleBackColor = true;
+            // 
+            // TestForm
+            // 
+            this.ClientSize = new System.Drawing.Size(292, 261);
+            this.Controls.Add(this.answerRadioButton3);
+            this.Controls.Add(this.answerRadioButton2);
+            this.Controls.Add(this.answerRadioButton1);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.questionLabel);
+            this.Name = "TestForm";
+            this.Load += new System.EventHandler(this.TestForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ test/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class TestForm : Form
+    {
+        private List<Question> questions;
+        private int currentQuestionIndex;
+        private int correctAnswersCount;
+
+        public TestForm()
+        {
+            InitializeComponent();
+            Load += TestForm_Load;
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string questionsFilePath = "C:\\Users\\суперкомп\\source\\repos\\test\\test\\questions.txt";
+            // Загрузка вопросов из файла при загрузке формы
+            LoadQuestionsFromFile(questionsFilePath);
+
+            answerRadioButton1.CheckedChanged += answerRadio_CheckedChanged;
+            answerRadioButton2.CheckedChanged += answerRadio_CheckedChanged;
+            answerRadioButton3.CheckedChanged += answerRadio_CheckedChanged;
+
+            // Начало теста
+            StartTest();
+        }
+
+        private void LoadQuestionsFromFile(string fileName)
+        {
+            questions = new List<Question>();
+
+            if (File.Exists(fileName))
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // Парсинг строк из файла и создание объектов вопросов
+                        // Формат файла: вопрос;ответ1;ответ2;ответ3;правильный_ответ
+                        string[] parts = line.Split(';');
+                        if (parts.Length == 5)
+                        {
+                            string questionText = parts[0];
+                            string[] answerOptions = new string[] { parts[1], parts[2], parts[3] };
+                            int correctAnswerIndex = int.Parse(parts[4]);
+                            Question question = new Question(questionText, answerOptions, correctAnswerIndex);
+                            questions.Add(question);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл с вопросами не найден.");
+            }
+        }
+
+        private void StartTest()
+        {
+            // Инициализация начальных значений
+            currentQuestionIndex = 0;
+            correctAnswersCount = 0;
+
+            // Отображение первого вопроса
+            ShowQuestion(currentQuestionIndex);
+        }
+
+        private void ShowQuestion(int questionIndex)
+        {
+            // Проверка наличия вопроса по заданному индексу
+            if (questionIndex >= 0 && questionIndex < questions.Count)
+            {
+                Question question = questions[questionIndex];
+
+                // Отображение вопроса и вариантов ответов на форме
+                questionLabel.Text = question.Text;
+                answerRadioButton1.Text = question.AnswerOptions[0];
+                answerRadioButton2.Text = question.AnswerOptions[1];
+                answerRadioButton3.Text = question.AnswerOptions[2];
+
+                // Очистка выбранного ответа
+                answerRadioButton1.Checked = false;
+                answerRadioButton2.Checked = false;
+                answerRadioButton3.Checked = false;
+
+                // Обновление прогресс-бара
+                progressBar.Maximum = questions.Count;
+                progressBar.Value = questionIndex + 1;
+
+                // Переключение доступности кнопки "Ок"
+                okButton.Enabled = false;
+            }
+            else
+            {
+                // Если вопросы закончились, выводится окно результата
+                ShowTestResult();
+            }
+        }
+
+
+
+        private void ShowTestResult()
+        {
+            // Окно с результатами теста
+            MessageBox.Show($"Тест завершен.\nКоличество правильных ответов: {correctAnswersCount} из {questions.Count}");
+
+            // Закрытие формы
+            Close();
+        }
+
+        public void answerRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            // Переключение доступности кнопки "Ок" при выборе ответа
+            okButton.Enabled = true;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            // Проверка выбранного ответа
+            if (answerRadioButton1.Checked || answerRadioButton2.Checked || answerRadioButton3.Checked)
+            {
+                int selectedAnswerIndex = GetSelectedAnswerIndex();
+
+                // Проверка правильности ответа
+                if (selectedAnswerIndex == questions[currentQuestionIndex].CorrectAnswerIndex)
+                {
+                    correctAnswersCount++;
+                }
+
+                // Переход к следующему вопросу
+                currentQuestionIndex++;
+                ShowQuestion(currentQuestionIndex);
+            }
+            else
+            {
+                MessageBox.Show("Выберите один из вариантов ответа.");
+            }
+        }
+
+        private int GetSelectedAnswerIndex()
+        {
+            if (answerRadioButton1.Checked)
+            {
+                return 1;
+            }
+            else if (answerRadioButton2.Checked)
+            {
+                return 2;
+            }
+            else if (answerRadioButton3.Checked)
+            {
+                return 3;
+            }
+
+            // Вернуть значение по умолчанию или сгенерировать исключение, указывающее, что ответ не выбран.
+            return -1;
+        }
+
+    }
+
+    public class Question
+    {
+        public string Text { get; }
+        public string[] AnswerOptions { get; }
+        public int CorrectAnswerIndex { get; }
+
+        public Question(string text, string[] answerOptions, int correctAnswerIndex)
+        {
+            Text = text;
+            AnswerOptions = answerOptions;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+    }
+}
